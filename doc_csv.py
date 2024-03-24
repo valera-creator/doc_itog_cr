f82cb1ab635cc72a79966a08f05b0be288094975
Valera <valerylarionov06@gmail.com>
2024-03-24 15:30:27 +0300

    correct csv

--- doc_csv.py
+++ doc_csv.py
@@ -2,11 +2,12 @@ import csv
 
 # чтение из csv
 with open('file.csv', encoding="utf8") as csvfile:  # урок pyqt6, параграф 6
-    reader = list(csv.reader(csvfile, delimiter='-', quotechar='"'))[1:]
+    reader = list(csv.reader(csvfile, delimiter=';', quotechar='"'))[1:]  # отрезаем заголовки, delimiter - разделитель
+    print(reader)
 
 # Запись
 # Работа с простыми таблицами (csv). Работа с табличными данными в PyQT, параграф 6
 with open('file.csv', 'w', newline='', encoding="utf8") as csvfile:
     writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for elem in [[1, 2], [3, 4]]:
+    for elem in [['first', 'second'], [1, 2]]:
         writer.writerow(elem)
