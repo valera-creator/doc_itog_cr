74b73945e608652aa6dfcc167e5ddef14b8d6400
Valera <valerylarionov06@gmail.com>
2024-03-26 14:06:14 +0300

    correct csv

--- doc_server.py
+++ doc_server.py
@@ -2,6 +2,6 @@ import requests
 
 port = 5000
 host = '127.0.0.1'
-url = f'https://{host}:{port}'  # не забываем https://
+url = f'http://{host}:{port}'  # не забываем http://, если не робит, то s приписать попробовать
 response = requests.get(url)
 data_get = response.json()
